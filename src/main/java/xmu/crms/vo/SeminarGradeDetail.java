c838ece11f8e122125f46b43315ce6540b50d892
moonlight <1241621063@qq.com>
2017-12-26 19:19:54 +0800

    之前的修改

--- src/main/java/xmu/crms/vo/SeminarGradeDetail.java
+++ src/main/java/xmu/crms/vo/SeminarGradeDetail.java
@@ -3,25 +3,56 @@ package xmu.crms.vo;
 
 import xmu.crms.entity.SeminarGroup;
 
+import java.math.BigInteger;
+import java.util.List;
+
 public class SeminarGradeDetail {
 
     private String seminarName;
     private String groupName;
     private String leaderName;
-    private Integer presentationGrade;
-    private Integer reportGrade;
-    private Integer grade;
-
+    private List<PresentationGrade> presentationGrade;
+    private BigInteger reportGrade;
+    private BigInteger grade;
+    public SeminarGradeDetail(String seminarName, String groupName, String leaderName, List<PresentationGrade> presentationGrade, BigInteger reportGrade, BigInteger grade) {
+        this.seminarName = seminarName;
+        this.groupName = groupName;
+        this.leaderName = leaderName;
+        this.presentationGrade = presentationGrade;
+        this.reportGrade = reportGrade;
+        this.grade = grade;
+    }
     public SeminarGradeDetail(SeminarGroup seminarGroup) {
         this.seminarName = seminarGroup.getSeminar().getName();
         this.leaderName = seminarGroup.getLeader().getName();
-        this.presentationGrade = seminarGroup.getPresentationGrade();
-        this.reportGrade = seminarGroup.getReportGrade();
-        this.grade = seminarGroup.getFinalGrade();
+
     }
 
     public SeminarGradeDetail() {}
 
+    public List<PresentationGrade> getPresentationGrade() {
+        return presentationGrade;
+    }
+
+    public void setPresentationGrade(List<PresentationGrade> presentationGrade) {
+        this.presentationGrade = presentationGrade;
+    }
+
+    public BigInteger getReportGrade() {
+        return reportGrade;
+    }
+
+    public void setReportGrade(BigInteger reportGrade) {
+        this.reportGrade = reportGrade;
+    }
+
+    public BigInteger getGrade() {
+        return grade;
+    }
+
+    public void setGrade(BigInteger grade) {
+        this.grade = grade;
+    }
 
     public String getSeminarName() {
         return seminarName;
@@ -45,30 +76,6 @@ public class SeminarGradeDetail {
 
     public void setLeaderName(String leaderName) {
         this.leaderName = leaderName;
-    }
-
-    public Integer getPresentationGrade() {
-        return presentationGrade;
-    }
-
-    public void setPresentationGrade(Integer presentationGrade) {
-        this.presentationGrade = presentationGrade;
-    }
-
-    public Integer getReportGrade() {
-        return reportGrade;
-    }
-
-    public void setReportGrade(Integer reportGrade) {
-        this.reportGrade = reportGrade;
-    }
-
-    public Integer getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Integer grade) {
-        this.grade = grade;
     }
 
     @Override
